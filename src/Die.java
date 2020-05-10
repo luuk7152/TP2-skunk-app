92f0d678589a60c2cef9f193f52994f15c822396
Rhyan Foo Kune <rfookune@gmail.com>
2020-05-10 18:52:22 -0500

    added test for die class

---
+++ src/Die.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Die {
+
+	@Test
+	public void generate_random_number_between_1_and_6()
+	{
+		
+	}
+	
+	@Test
+	public void get_last_roll() 
+	{
+		
+	}
+	
+	@Test
+	public void display_class_toString_message() 
+	{
+		
+	}
+	
+}
