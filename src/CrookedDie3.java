3511b905f4c58890a61a14b4f6c336438a2a81fe
luuk7152 <luuk7152@stthomas.edu>
2020-05-12 22:31:27 -0500

    Created TestCrookedDie3 with tests and CrookedDie3 without methods yet

---
+++ src/CrookedDie3.java
@@ -0,0 +1,20 @@
+public class CrookedDie3 extends Die
+{
+	public CrookedDie3()
+	{
+		
+	}
+	
+	@Override
+	public int getLastRoll()
+	{
+		return 0;
+	}
+
+	@Override
+	public String toString()
+	{
+		return null;
+	}
+
+}
---
+++ test/TestCrookedDie3.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestCrookedDie3 {
+	private CrookedDie3 crDie;
+	//getLastRoll
+	
+	@Test
+	void test_Get_Last_Roll_Cr_Die3()
+	{
+		crDie = new CrookedDie3();
+		assertEquals(crDie.getLastRoll(), 3);
+		
+	}
+	
+	@Test
+	void test_To_String_Cr_Die3()
+	{
+		crDie = new CrookedDie3();
+		assertEquals(crDie.toString(), "A CrookedDie3 always rolling 3");
+	}
+}
