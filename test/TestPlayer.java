d901c3418e189545a18618db4eb0a5ba6538e405
Rhyan Foo Kune <rfookune@gmail.com>
2020-05-13 19:43:14 -0500

    added chip count getter and setter

--- test/TestPlayer.java
+++ test/TestPlayer.java
@@ -19,17 +19,37 @@ public class TestPlayer {
 		assertEquals(player.getClass().getName(), "Player");
 	}
 	
+	@Test
 	void test_get_name()
 	{
 		assertEquals(player.getName(), name);
 	}
 	
+	@Test
 	void test_set_name()
 	{
 		String newName = "Mary Johnson";
 		player.setName(newName);
 		
 		assertEquals(player.getName(), newName);
+	}
+	
+	@Test
+	void test_get_chip_count()
+	{
+		int chipCount = 100;
+		player = new Player(name, chipCount);
+		
+		assertEquals(player.getChipCount(), chipCount);
+	}
+	
+	@Test
+	void test_set_chip_count()
+	{
+		int newChipCount = 10;
+		player.setChipCount(newChipCount);
+		
+		assertEquals(player.getChipCount(), newChipCount);
 	}
 	
 }
