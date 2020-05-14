a8c62434d1774565bbb16593fc9caafa846e5750
Rhyan Foo Kune <rfookune@gmail.com>
2020-05-13 19:39:17 -0500

    added player setter and getter for name

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,27 @@
 
 public class Player {
 
+	private String name;
+	private int chipCount;
+	
+	
+	// constructors
+	Player(String name)
+	{
+		this.name = name;
+		this.chipCount = 50; // starting chip count 
+	}
+	
+	
+	// getters and setters
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
 }
