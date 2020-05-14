0154ef96254aed73ca11f9ca379dfe0c0e2c9534
Rhyan Foo Kune <rfookune@gmail.com>
2020-05-13 23:57:19 -0500

    update package name to adhere to naming standards

---
+++ src/com/app/skunk/CrookedDie2.java
@@ -0,0 +1,16 @@
+package com.app.skunk;
+public class CrookedDie2 extends Die
+{	
+	@Override
+	public int getLastRoll()
+	{
+		return 2;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "A CrookedDie2 always rolling 2";
+	}
+
+}
---
+++ src/com/app/skunk/CrookedDie3.java
@@ -0,0 +1,16 @@
+package com.app.skunk;
+public class CrookedDie3 extends Die
+{
+	@Override
+	public int getLastRoll()
+	{
+		return 3;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "A CrookedDie3 always rolling 3";
+	}
+
+}
