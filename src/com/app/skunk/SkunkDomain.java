dfa687dac66845393654b959beb8cd54c1f747ab
Rhyan Foo Kune <rfookune@gmail.com>
2020-05-14 14:31:53 -0500

    refactor skunk domain to decrease complexity

--- src/com/app/skunk/SkunkDomain.java
+++ src/com/app/skunk/SkunkDomain.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 public class SkunkDomain {
 
 	public Interface ui;
+	
 	private ArrayList<Player> players;
 	private int kitty;
 	
-	private int TARGET_SCORE = 20;
+	private int TARGET_SCORE = 100;
 	
 	public SkunkDomain(Interface ui)
 	{
@@ -24,16 +25,7 @@ public class SkunkDomain {
 	{
 		ui.welcomeMessage();
 		
-		Integer playerCount = ui.promptPlayerCount();
-		
-		for (int index = 1; index <= playerCount; index++) {
-			
-			String playerName = ui.promptPlayerName(index);
-			Player player = new Player(playerName);
-			
-			this.players.add(player);
-		}
-		
+		this.setupPlayers();
 		
 		//start a new game
 		Game game = new Game(this.players);
@@ -42,169 +34,16 @@ public class SkunkDomain {
 		Player currentPlayer = null;
 		
 		while (!gameEnded) {
-			
 			currentPlayer = game.getCurrentPlayer();
-			
-			//start new turn
-			Turn turn = new Turn();
-			currentPlayer.setCurrentTurn(turn);
-			
-			ui.turnStartedMessage(currentPlayer);
-			
-			String playerDecision = "";
-			Boolean skunkRolled = false;
-			
-			//do while player chooses to roll
-			do {
-				
-				//ask player's to roll or pass
-				playerDecision = ui.promptRollOrPass(currentPlayer.getName()).toLowerCase();
-				
-				//handle player's decision
-				switch (playerDecision) 
-				{
-					case "r":
-						
-						//roll dice and handle roll
-						turn.roll();
-						
-						if (turn.isSkunked()) {
-							
-							skunkRolled = true;
-							
-							currentPlayer.handleSkunk();
-							
-							int penalty = turn.getPenalty();
-							kitty = kitty + penalty;
-							currentPlayer.setChipCount(currentPlayer.getChipCount() - penalty);
-							
-							game.endTurn();
-							
-							ui.skunkRolledMessage();
-						}
-						
-						ui.turnRollReport(currentPlayer, turn);
-						
-						break;
-						
-					case "p":
-						
-						//end player's turn
-						game.endTurn();
-						ui.endTurnMessage(currentPlayer.getName());
-						break;
-					
-					default: 
-						ui.invalidInputMessage(); //handle invalid user input
-						break;
-				}
-				
-			} while (!"p".equals(playerDecision) && !skunkRolled);
-			
-			
-			//player reached target score - end game
-			if (currentPlayer.getScore() >= TARGET_SCORE) {
-				gameEnded = true; 
-				game.setWinner(currentPlayer);
-				ui.targetScoreReachedMessage(currentPlayer);
-			}
-			
-			boolean validSummaryInput = false;
-			
-			while (!validSummaryInput) {
-	
-				String showSummary = ui.promptShowTurnSummary();
-			
-				switch (showSummary)
-				{
-					case "y":
-						ui.turnReport(currentPlayer, turn);
-						validSummaryInput = true;
-						break;
-						
-					case "n":
-						validSummaryInput = true;
-						break;
-					
-					default: 
-						ui.invalidInputMessage(); //handle invalid user input
-						break;
-				}
-				
-			}
-			
+			gameEnded = this.playATurn(game, currentPlayer, false);
 			game.setNextPlayer();
 		}
 		
 		
 		// game ended - allow one more turn for each non winner
 		while (game.getCurrentPlayer() != game.getFirstWinner()) {
-			
 			currentPlayer = game.getCurrentPlayer();
-			
-			Turn turn = new Turn();
-			currentPlayer.setCurrentTurn(turn);
-			
-			ui.lastTurnStartedMessage(currentPlayer);
-			
-			String playerDecision = "";
-			Boolean skunkRolled = false;
-			
-			//do while player chooses to roll
-			do {
-				
-				//ask player's to roll or pass
-				playerDecision = ui.promptRollOrPass(currentPlayer.getName()).toLowerCase();
-				
-				//handle player's decision
-				switch (playerDecision) 
-				{
-					case "r":
-						
-						//roll dice and handle roll
-						turn.roll();
-						
-						if (turn.isSkunked()) {
-							
-							skunkRolled = true;
-							
-							currentPlayer.handleSkunk();
-							
-							int penalty = turn.getPenalty();
-							kitty = kitty + penalty;
-							currentPlayer.setChipCount(currentPlayer.getChipCount() - penalty);
-							
-							game.endTurn();
-							
-							ui.skunkRolledMessage();
-						}
-						
-						ui.turnRollReport(currentPlayer, turn);
-						
-						break;
-						
-					case "p":
-						
-						//end player's turn
-						game.endTurn();
-						ui.endTurnMessage(currentPlayer.getName());
-						break;
-					
-					default: 
-						ui.invalidInputMessage(); //handle invalid user input
-						break;
-				}
-				
-			} while (!"p".equals(playerDecision) && !skunkRolled);
-			
-			
-			// if player reached target score
-			if (currentPlayer.getScore() > game.getWinner().getScore()) {
-				Player previousWinner = game.getWinner();
-				game.setWinner(currentPlayer);
-				ui.newWinnerMessage(currentPlayer, previousWinner);
-			}
-
+			this.playATurn(game, currentPlayer, true);
 			game.setNextPlayer();
 		}
 		
@@ -216,5 +55,134 @@ public class SkunkDomain {
 		
 		// Game ended
 		ui.displayGameEndedMessage();
+	}
+	
+	private void setupPlayers()
+	{
+		Integer playerCount = ui.promptPlayerCount();
+		
+		for (int index = 1; index <= playerCount; index++) {
+			
+			String playerName = ui.promptPlayerName(index);
+			Player player = new Player(playerName);
+			
+			this.players.add(player);
+		}
+	}
+	
+	private Boolean playATurn(Game game, Player currentPlayer, Boolean lastTurn)
+	{
+		Boolean gameEnded = false;
+		
+		//start new turn
+		Turn turn = new Turn();
+		currentPlayer.setCurrentTurn(turn);
+		
+		if (lastTurn) {
+			ui.lastTurnStartedMessage(currentPlayer);
+		} else {
+			ui.turnStartedMessage(currentPlayer);
+		}
+		
+		String playerDecision = "";
+		Boolean skunkRolled = false;
+		
+		
+		//do while player chooses to roll
+		do {
+			
+			//ask player's to roll or pass
+			playerDecision = ui.promptRollOrPass(currentPlayer.getName()).toLowerCase();
+			
+			//handle player's decision
+			switch (playerDecision) 
+			{
+				case "r":
+					
+					//roll dice and handle roll
+					turn.roll();
+					
+					if (turn.isSkunked()) {
+						
+						skunkRolled = true;
+						
+						currentPlayer.handleSkunk();
+						
+						int penalty = turn.getPenalty();
+						kitty = kitty + penalty;
+						currentPlayer.setChipCount(currentPlayer.getChipCount() - penalty);
+						
+						game.endTurn();
+						
+						ui.skunkRolledMessage();
+					}
+					
+					ui.turnRollReport(currentPlayer, turn);
+					
+					break;
+					
+				case "p":
+					
+					//end player's turn
+					game.endTurn();
+					ui.endTurnMessage(currentPlayer.getName());
+					break;
+				
+				default: 
+					ui.invalidInputMessage(); //handle invalid user input
+					break;
+			}
+			
+		} while (!"p".equals(playerDecision) && !skunkRolled);
+		
+		
+		if (lastTurn) {
+			
+			if (currentPlayer.getScore() > game.getWinner().getScore()) { // if player reached previous winner score - set as new winner
+				Player previousWinner = game.getWinner();
+				game.setWinner(currentPlayer);
+				ui.newWinnerMessage(currentPlayer, previousWinner);
+			}
+			
+		} else {
+			
+			if (currentPlayer.getScore() >= TARGET_SCORE) { //player reached target score - end the game
+				game.setWinner(currentPlayer);
+				ui.targetScoreReachedMessage(currentPlayer);
+				gameEnded = true; 
+			}
+			
+		}
+		
+		this.showTurnSummaryOption(currentPlayer, turn);
+		
+		return gameEnded;
+	}
+	
+	private void showTurnSummaryOption(Player player, Turn turn)
+	{
+		Boolean validSummaryInput = false;
+		
+		while (!validSummaryInput) {
+			
+			String showSummary = ui.promptShowTurnSummary();
+		
+			switch (showSummary)
+			{
+				case "y":
+					ui.turnReport(player, turn);
+					validSummaryInput = true;
+					break;
+					
+				case "n":
+					validSummaryInput = true;
+					break;
+				
+				default: 
+					ui.invalidInputMessage(); //handle invalid user input
+					break;
+			}
+			
+		}
 	}
 }
