83f1723d3ce0cb43fd729ff18dc675043fc71fe3
Rhyan Foo Kune <rfookune@gmail.com>
2020-05-14 11:30:48 -0500

    added game unit tests

--- test/com/app/skunk/TestGame.java
+++ test/com/app/skunk/TestGame.java
@@ -1,6 +1,9 @@
 package com.app.skunk;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,49 @@ public class TestGame {
 	void testGameConstructor()
 	{
 		assertEquals(game.getClass().getSimpleName(), "Game");
+		
+		ArrayList<Player> players = new ArrayList<Player>();
+		for (int i = 1; i < 50; i++) {
+			String name = Integer.toString(i);
+			Player player = new Player(name);
+			players.add(player);
+        }
+		
+		game = new Game(players);
+		assertTrue(game.getPlayers().size() == players.size());
 	}
 	
+	@Test
+	void testSetPlayers()
+	{
+		ArrayList<Player> players = new ArrayList<Player>();
+		for (int i = 1; i < 50; i++) {
+			String name = Integer.toString(i);
+			Player player = new Player(name);
+			players.add(player);
+        }
+		
+		game.setPlayers(players);
+		assertTrue(game.getPlayers().size() == players.size());
+	}
+	
+	@Test
+	void testGetPlayers()
+	{
+		String name1 = "John Doe";
+		String name2 = "Mary Johnson";
+		
+		Player player1 = new Player(name1);
+		Player player2 = new Player(name2);
+		
+		ArrayList<Player> players = new ArrayList<Player>();
+		
+		players.add(player1);
+		players.add(player2);
+		
+		game.setPlayers(players);
+		
+		assertEquals(game.getPlayers().get(0).getName(), name1);
+		assertEquals(game.getPlayers().get(1).getName(), name2);
+	}
 }
