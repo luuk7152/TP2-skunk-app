ef7976cda18076a20b745b7301f9d233b8a6e14a
Rhyan Foo Kune <rfookune@gmail.com>
2020-05-14 12:27:50 -0500

    added ui layer and domain-ui layer connection class

---
+++ src/com/app/skunk/SkunkApp.java
@@ -0,0 +1,28 @@
+package com.app.skunk;
+
+public class SkunkApp {
+
+	public Interface ui;
+	public SkunkDomain skunkDomain;;
+	
+	
+	public SkunkApp()
+	{
+		// Create UI layer
+		ui = new Interface();
+		
+		// Create domain layer
+		skunkDomain = new SkunkDomain(ui);
+	}
+	
+	public void run()
+	{
+		skunkDomain.run();
+	}
+	
+	public static void main(String[] args) 
+	{
+		new SkunkApp().run();
+	}
+
+}
---
+++ src/com/app/skunk/SkunkDomain.java
@@ -0,0 +1,17 @@
+package com.app.skunk;
+
+public class SkunkDomain {
+
+	public Interface ui;
+	
+	public SkunkDomain(Interface ui)
+	{
+		this.ui = ui;
+	}
+	
+	public void run()
+	{
+		//game logic
+		
+	}
+}
